5da570997de3087e306c69901f3c7ab0fb367915
Griffin Foster <griffin.foster@gmail.com>
2012-02-21 16:17:28 +0000

    shapelets: updated plotCoeffs.py

--- scripts/plotCoeffs.py
+++ scripts/plotCoeffs.py
@@ -16,23 +16,38 @@ if __name__ == '__main__':
     opts, args = o.parse_args(sys.argv[1:])
 
     ifn=args[0]
-    d=shapelets.fileio.readHermiteCoeffs(ifn)
+    d=shapelets.fileio.readCoeffs(ifn)
 
-    rx=n.array(range(0,d['size'][0]),dtype=float)-d['xc'][0]
-    ry=n.array(range(0,d['size'][1]),dtype=float)-d['xc'][1]
-    bvals=shapelets.decomp.genBasisMatrix(d['beta'],d['norder'],rx,ry)
-    mdl=shapelets.img.constructHermiteModel(bvals,d['coeffs'],d['xc'],d['size'])
+    if d['mode'].startswith('herm'):
+        rx=n.array(range(0,d['size'][0]),dtype=float)-d['xc'][0]
+        ry=n.array(range(0,d['size'][1]),dtype=float)-d['xc'][1]
+        bvals=shapelets.decomp.genBasisMatrix(d['beta'],d['norder'],rx,ry)
+        mdl=shapelets.img.constructModel(bvals,d['coeffs'],d['xc'],d['size'])
+        p.suptitle('Hermite')
+    elif d['mode'].startswith('lag'):
+        r0,th0=shapelets.shapelet.polarArray(d['xc'],d['size'])
+        bvals=shapelets.decomp.genPolarBasisMatrix(d['beta'],d['norder'],r0,th0)
+        mdl=n.abs(shapelets.img.constructModel(bvals,d['coeffs'],d['xc'],d['size']))
+        coeffs=d['coeffs']
+        cimR=shapelets.img.polarCoeffImg(coeffs.real,d['norder'])
+        cimI=shapelets.img.polarCoeffImg(coeffs.imag,d['norder'])
+        cimI=n.fliplr(cimI)
+        cim=n.concatenate((cimR,cimI),axis=1)
+        p.suptitle('Laguerre')
     
     p.subplot(121)
     p.title('Model')
     p.imshow(mdl)
-    p.text(d['xc'][0],d['xc'][1],'+')
+    p.text(d['xc'][1],d['xc'][0],'+')
     p.colorbar()
 
     p.subplot(122)
     p.title('Coefficents')
-    coeffs=n.reshape(d['coeffs'],d['norder'])
-    p.pcolor(coeffs)
+    if d['mode'].startswith('herm'):
+        coeffs=n.reshape(d['coeffs'],d['norder'])
+        p.pcolor(coeffs)
+    elif d['mode'].startswith('lag'):
+        p.pcolor(cim)
     p.colorbar()
     
     p.show()
