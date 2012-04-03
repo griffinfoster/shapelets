2f8e1fd5f47ff9540ba4f882eb13f19845e59e9c
Griffin Foster <griffin@poseidon-empty.(none)>
2012-04-03 17:55:34 +0100

    correctly Fourier transform shapelet basis functions

--- scripts/plotCoeffs.py
+++ scripts/plotCoeffs.py
@@ -19,15 +19,53 @@ if __name__ == '__main__':
     d=shapelets.fileio.readCoeffs(ifn)
 
     if d['mode'].startswith('herm'):
+        #model
         rx=n.array(range(0,d['size'][0]),dtype=float)-d['xc'][0]
         ry=n.array(range(0,d['size'][1]),dtype=float)-d['xc'][1]
+        #print rx
+        #print ry
+        #print len(rx),len(ry)
         bvals=shapelets.decomp.genBasisMatrix(d['beta'],d['norder'],rx,ry)
-        mdl=shapelets.img.constructModel(bvals,d['coeffs'],d['xc'],d['size'])
+        mdl=shapelets.img.constructModel(bvals,d['coeffs'],d['size'])
+        
+        #ft
+        xsize=(float(len(ry))/float(len(rx)))*1./n.abs(rx[0]-rx[1]) #!!!
+        ysize=(float(len(rx))/float(len(ry)))*1./n.abs(ry[0]-ry[1]) #!!!
+        #print xsize,ysize
+        xres=(float(len(ry))/float(len(rx)))*1./n.abs(rx[0]-rx[-1]) #!!!
+        yres=(float(len(rx))/float(len(ry)))*1./n.abs(ry[0]-ry[-1]) #!!!
+        #print xres,yres
+        xmin=xres*(rx[0]+.5)
+        ymin=yres*(ry[0]+.5)
+        uu=n.arange(0,xsize+xres,xres)+xmin
+        vv=n.arange(0,ysize+yres,yres)+ymin
+        #print len(uu),len(vv)
+        #print uu,vv
+        bvals=[]
+        basis=shapelets.shapelet.ftHermiteBasis(d['beta'],d['norder'])
+        for bf in basis:
+            bvals.append(shapelets.shapelet.computeBasis2d(bf,uu,vv).flatten())
+        bm=n.array(bvals)
+        mdl_ft=shapelets.img.constructModel(bm.transpose(),d['coeffs'],[len(uu),len(vv)])
+        #mdl_ft=mdl_ft.real
+
+        #uu=n.array(range(0,d['size'][0]),dtype=float)-d['xc'][0]
+        #vv=n.array(range(0,d['size'][1]),dtype=float)-d['xc'][1]
+        ##print uu
+        ##print vv
+        #bvals=[]
+        #basis=shapelets.shapelet.ftHermiteBasis([1./d['beta'][0],1./d['beta'][1]],d['norder'])
+        #for bf in basis:
+        #    bvals.append(shapelets.shapelet.computeBasis2d(bf,uu,vv).flatten())
+        #bm=n.array(bvals)
+        #mdl_ft=shapelets.img.constructModel(bm.transpose(),d['coeffs'],[len(uu),len(vv)])
+        #mdl_ft=mdl_ft.real
+
         p.suptitle('Hermite')
     elif d['mode'].startswith('lag'):
         r0,th0=shapelets.shapelet.polarArray(d['xc'],d['size'])
         bvals=shapelets.decomp.genPolarBasisMatrix(d['beta'],d['norder'],r0,th0)
-        mdl=n.abs(shapelets.img.constructModel(bvals,d['coeffs'],d['xc'],d['size']))
+        mdl=n.abs(shapelets.img.constructModel(bvals,d['coeffs'],d['size']))
         coeffs=d['coeffs']
         cimR=shapelets.img.polarCoeffImg(coeffs.real,d['norder'])
         cimI=shapelets.img.polarCoeffImg(coeffs.imag,d['norder'])
@@ -35,13 +73,35 @@ if __name__ == '__main__':
         cim=n.concatenate((cimR,cimI),axis=1)
         p.suptitle('Laguerre')
     
-    p.subplot(121)
+    p.subplot(131)
     p.title('Model')
     p.imshow(mdl)
     p.text(d['xc'][1],d['xc'][0],'+')
-    p.colorbar()
+    #p.colorbar()
 
-    p.subplot(122)
+    #p.subplot(234)
+    #mdl_fft2=n.fft.fft2(mdl)
+    #axis0_shift=int(mdl_fft2.shape[0]/2)
+    #axis1_shift=int(mdl_fft2.shape[1]/2)
+    #mdl_fft2=n.concatenate((mdl_fft2[axis0_shift:,:],mdl_fft2[:axis0_shift,:]),axis=0)
+    #mdl_fft2=n.concatenate((mdl_fft2[:,axis1_shift:],mdl_fft2[:,:axis1_shift]),axis=1)
+    #p.imshow(n.abs(mdl_fft2))
+
+    p.subplot(132)
+    p.title('Fourier Transform')
+    p.imshow(mdl_ft.real)
+    #p.imshow(n.abs(mdl_ft))
+    #p.colorbar()
+
+    #p.subplot(235)
+    #mdl_ft_fft2=n.fft.fft2(mdl_ft)
+    #axis0_shift=int(mdl_ft_fft2.shape[0]/2)
+    #axis1_shift=int(mdl_ft_fft2.shape[1]/2)
+    #mdl_ft_fft2=n.concatenate((mdl_ft_fft2[axis0_shift:,:],mdl_ft_fft2[:axis0_shift,:]),axis=0)
+    #mdl_ft_fft2=n.concatenate((mdl_ft_fft2[:,axis1_shift:],mdl_ft_fft2[:,:axis1_shift]),axis=1)
+    #p.imshow(n.abs(mdl_ft_fft2))
+
+    p.subplot(133)
     p.title('Coefficents')
     if d['mode'].startswith('herm'):
         coeffs=n.reshape(d['coeffs'],d['norder'])
