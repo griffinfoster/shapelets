42aa5c2747f0929a08a51a4dee74db13fdaeb486
Griffin Foster <griffin.foster@gmail.com>
2015-05-22 14:10:39 +0200

    fitShapelet.py superedes fitBeta.py, fitHermite.py, and fitLaguerre.py

--- scripts/test_scripts.sh
+++ scripts/test_scripts.sh
@@ -6,4 +6,5 @@ solveShapelet.py ../data/N6251_test.fits -r 1028,1097,1025,1074 -N 967,1067,972,
 solveShapelet.py ../data/N6251_test.fits -r 1028,1097,1025,1074 -N 967,1067,972,1026 -n 20 -b 3.,3. -p 0.448266
 
 fitShapelet.py ../data/N6251_test.fits -r 1028,1097,1025,1074 -N 967,1067,972,1026 -n 8 -m polar -p 0.486636 -b 4.,4. --set_phi --set_xc
+fitShapelet.py ../data/N6251_test.fits -r 1028,1097,1025,1074 -N 967,1067,972,1026 -n 8 -p 0.486636 -b 4.,4. --set_phi --set_xc
 
