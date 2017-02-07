557f5999c003f3279d692e7d8f7176f158077b5c
Griffin Foster <griffin.foster@gmail.com>
2017-02-07 12:55:58 +0000

    added test for python-casacore

--- shapelets/phs/__init__.py
+++ shapelets/phs/__init__.py
@@ -2,5 +2,16 @@
 A module interface to Measurement Set phase rotation functions
 """
 
-import ClassMS, ModColor, ModRotate, rad2hmsdms, reformat
+import imp
+
+try:
+    imp.find_module('casacore')
+    casacore = True
+except ImportError:
+    casacore = False
+
+if casacore:
+    import ClassMS, ModRotate # these modules require casacore
+
+import ModColor, rad2hmsdms, reformat
 
